efdfdb9fb8f276b84a63c6fdb1d985667a78daf1
Tomas Babej <tomasbabej@gmail.com>
2015-10-31 14:43:27 +0100

    knowledge: Add method to add new note to Anki

--- knowledge/knowledge.py
+++ knowledge/knowledge.py
@@ -8,4 +8,20 @@ BASE_DIR = vim.eval("s:plugin_path")
 sys.path.insert(0, os.path.join(BASE_DIR, 'knowledge'))
 
 from proxy import AnkiProxy
+from wikinote import WikiNote
+
 proxy = AnkiProxy(os.path.expanduser('~/Documents/Anki/User 1/collection.anki2'))
+
+def create_notes():
+    """
+    Loops over current buffer and adds any new notes to Anki.
+    """
+
+    for line_number in range(len(vim.current.buffer)):
+        note = WikiNote.from_line(line_number, proxy)
+
+        if note is None:
+            continue
+
+        if not note.created:
+            note.save()
