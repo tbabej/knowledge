614f2807ba165b587b97fa7c897f711d58d57750
Tomas Babej <tomasbabej@gmail.com>
2016-04-17 20:44:44 +0200

    knowledge: Source SRS software provider from config variables

--- knowledge/knowledge.py
+++ knowledge/knowledge.py
@@ -20,12 +20,27 @@ def output_exception(original_hook, exception_type, value, tb):
 # Wrap the original except hook
 sys.excepthook = lambda a,b,c: output_exception(sys.excepthook, a, b, c)
 
-
 from proxy import AnkiProxy, MnemosyneProxy
 from wikinote import WikiNote, Header
 
-#proxy = AnkiProxy(os.path.expanduser('~/Documents/Anki/User 1/collection.anki2'))
-proxy = MnemosyneProxy()
+SRS_PROVIDER = vim.vars.get('knowledge_srs_provider')
+DATA_DIR = vim.vars.get('knowledge_data_dir')
+proxy = None
+
+if SRS_PROVIDER == 'Anki':
+    proxy = AnkiProxy(DATA_DIR)
+elif SRS_PROVIDER == 'Mnemosyne':
+    proxy = MnemosyneProxy(DATA_DIR)
+elif SRS_PROVIDER is None:
+    raise error.KnowledgeException(
+        "Variable knowledge_srs_provider has to have "
+        "one of the following values: Anki, Mnemosyne"
+    )
+else:
+    raise error.KnowledgeException(
+        "SRS provider '{0}' is not supported."
+        .format(SRS_PROVIDER)
+    )
 
 
 class HeaderStack(object):
