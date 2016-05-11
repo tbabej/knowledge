441813e96708eb8c646772e45ed98a5c83d637ac
Tomas Babej <tomasbabej@gmail.com>
2016-05-11 20:31:23 +0200

    tests: Expand header test suite

--- tests/test_header.py
+++ tests/test_header.py
@@ -27,3 +27,88 @@ class TestCreateBasicNoteUnderHeader(IntegrationTest):
 
     def execute(self):
         self.command("w", regex="written$", lines=1)
+
+
+class TestCreateClozeNoteUnderHeader(IntegrationTest):
+
+    viminput = """
+    === Life questions @ deck:Physics ===
+
+    The visible spectrum is between [390 to 700] nm.
+    """
+
+    vimoutput = """
+    === Life questions @ deck:Physics ===
+
+    The visible spectrum is between [390 to 700] nm. {identifier}
+    """
+
+    notes = [
+        dict(
+            text="The visible spectrum is between [390 to 700] nm.",
+            tags=['Physics']
+        )
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+
+
+class TestCreateBasicNoteUnderHeaderWithSubdeck(IntegrationTest):
+
+    viminput = """
+    === Life questions @ deck:Life.Questions ===
+
+    Q: This is a question
+    - And this is the answer
+    """
+
+    vimoutput = """
+    === Life questions @ deck:Life.Questions ===
+
+    Q: This is a question {identifier}
+    - And this is the answer
+    """
+
+    notes = [
+        dict(
+            front='This is a question',
+            back='And this is the answer',
+            tags=['Life::Questions']
+        )
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+
+
+class TestCreateBasicNoteUnderNestedHeadersOverriden(IntegrationTest):
+
+    viminput = """
+    == Universe @ deck:Universe ==
+
+    === Life @ deck:Life ===
+
+    Q: This is a question
+    - And this is the answer
+    """
+
+    vimoutput = """
+    == Universe @ deck:Universe ==
+
+    === Life @ deck:Life ===
+
+    Q: This is a question {identifier}
+    - And this is the answer
+    """
+
+    notes = [
+        dict(
+            front='This is a question',
+            back='And this is the answer',
+            tags=['Life']
+        )
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
