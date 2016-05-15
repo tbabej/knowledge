8a38160f4c1c0092bd8c67b7640579acb2a29ddf
Tomas Babej <tomasbabej@gmail.com>
2016-05-15 17:05:06 +0200

    tests: Extend header test suite

--- tests/test_header.py
+++ tests/test_header.py
@@ -112,3 +112,91 @@ class TestCreateBasicNoteUnderNestedHeadersOverriden(IntegrationTest):
 
     def execute(self):
         self.command("w", regex="written$", lines=1)
+
+
+class TestTokenInterpretedAsDeckByDefault(IntegrationTest):
+
+    viminput = """
+    === Life questions @ Life ===
+
+    Q: This is a question
+    - And this is the answer
+    """
+
+    vimoutput = """
+    === Life questions @ Life ===
+
+    Q: This is a question {identifier}
+    - And this is the answer
+    """
+
+    notes = [
+        dict(
+            front='This is a question',
+            back='And this is the answer',
+            tags=['Life']
+        )
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+
+
+class TestHeaderExtraTag(IntegrationTest):
+
+    viminput = """
+    === Life questions @ Life +extra ===
+
+    Q: This is a question
+    - And this is the answer
+    """
+
+    vimoutput = """
+    === Life questions @ Life +extra ===
+
+    Q: This is a question {identifier}
+    - And this is the answer
+    """
+
+    notes = [
+        dict(
+            front='This is a question',
+            back='And this is the answer',
+            tags=['Life', 'extra']
+        )
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+
+
+class TestHeaderStacking(IntegrationTest):
+
+    viminput = """
+    == Math formulas @ Math +formulas ==
+
+    === Life questions @ Math.Goniometric ===
+
+    Q: This is a question
+    - And this is the answer
+    """
+
+    vimoutput = """
+    == Math formulas @ Math +formulas ==
+
+    === Life questions @ Math.Goniometric ===
+
+    Q: This is a question {identifier}
+    - And this is the answer
+    """
+
+    notes = [
+        dict(
+            front='This is a question',
+            back='And this is the answer',
+            tags=['Math::Goniometric', 'formulas']
+        )
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
