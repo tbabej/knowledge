dc36ae0a5de0c97887ae0d480ac2898fe686a704
Tomas Babej <tomasbabej@gmail.com>
2016-05-17 00:00:32 +0200

    tests: Add basic test for close lists

--- tests/test_wikinote.py
+++ tests/test_wikinote.py
@@ -243,6 +243,39 @@ class TestCreateClozeNoteParagraphReformatting(IntegrationTest):
         self.command("w", regex="written$", lines=1)
 
 
+class TestCreateClozeItemNote(IntegrationTest):
+
+    viminput = """
+    List of capitols:
+    * England - [London]
+    * France - [Paris]
+    * Czech Republic - [Prague]
+    """
+
+
+    vimoutput = """
+    List of capitols:
+    * England - [London] {identifier}
+    * France - [Paris] {identifier}
+    * Czech Republic - [Prague] {identifier}
+    """
+
+    notes = [
+        dict(
+            text='List of capitols:\n* England - [London]'
+        ),
+        dict(
+            text='List of capitols:\n* France - [Paris]'
+        ),
+        dict(
+            text='List of capitols:\n* Czech Republic - [Prague]'
+        ),
+    ]
+
+    def execute(self):
+        self.command("w", regex="written$", lines=1)
+
+
 class TestOpenDollarSign(IntegrationTest):
 
     viminput = """
