247921b4f06378f1786d8ccb28cc154e803ec801
Tomas Babej <tomas@tbabej.com>
2018-07-25 19:14:02 -0400

    tests: Add conftest.py implementing the failure_log fixture

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
+
+
+@pytest.yield_fixture
+def failure_log(request):
+    messages = []
+    yield messages.append
+    item = request.node
+    if item.rep_call.failed:
+        print(f'\nAdditional details: {item.nodeid}')
+        for message in messages:
+            print (f"- {message}")
