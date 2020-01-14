ff5c8a9f3d245ff74e68cb1692a90c219a011592
Tomas Babej <tomas@tbabej.com>
2020-01-13 23:48:46 -0500

    utils: Add functionality to obtain values of vim variables

--- knowledge/utils.py
+++ knowledge/utils.py
@@ -1,5 +1,55 @@
+import glob
+import re
 import subprocess
 import vim
+
+
+NEOVIM = (vim.eval('has("nvim")') == "1")
+
+
+def decode_bytes(var):
+    """
+    Data structures obtained from vim under python3 will return bytestrings.
+    Neovim under python3 will return str.
+    Make sure we can handle that.
+    """
+
+    if NEOVIM:
+        return var
+
+    if isinstance(var, bytes):
+        return var.decode()
+
+    if isinstance(var, list):
+        return list([decode_bytes(element) for element in var])
+
+    if isinstance(var, dict) or 'vim.dictionary' in str(type(var)):
+        return  {
+            decode_bytes(key): decode_bytes(value)
+            for key, value in var.items()
+        }
+
+    return var
+
+
+def get_var(name, default=None, vars_obj=None):
+    """
+    Provide a layer for getting a variable value out of vim, consistent over
+    vim+py22/vim+py3/neovim combinations.
+
+    Params:
+        default - default value, returned when variable is not found
+        vars - used vars object, defaults to vim.vars
+    """
+
+    vars_obj = vars_obj or vim.vars
+    value = vars_obj.get(name)
+
+    if value is None:
+        return default
+    else:
+        return decode_bytes(value)
+
 
 def string_to_args(line):
     output = []
