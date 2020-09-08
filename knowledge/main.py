a54c88a1eff702d801c580542c6ab63a3329d746
Tomas Babej <tomas@tbabej.com>
2020-09-07 22:49:25 -0400

    latex: Support cloze hints

--- knowledge/main.py
+++ knowledge/main.py
@@ -375,8 +375,9 @@ def convert_to_pdf(interactive=False):
         lambda t: re.sub(r':\s*\n\* ', ':\n\n* ', t),
         # Add extra line to have enumerations recognized
         lambda t: re.sub(r':\s*\n(\d+)\. ', r':\n\n\1. ', t),
-        # Markup clozes as italics
-        lambda t: re.sub(r'\{(?P<cloze>[^\{\}]+)\}', r'\\knowledgeCloze{\g<cloze>}', t, flags=re.MULTILINE),
+        # Markup clozes as underlined OCGs
+        lambda t: re.sub(r'\{(?P<cloze>[^\{\}:]+)\}', r'\\knowledgeCloze{\g<cloze>}{}', t, flags=re.MULTILINE),
+        lambda t: re.sub(r'\{(?P<cloze>[^\{\}:]+):(?P<hint>[^\{\}]+)\}', r'\\knowledgeCloze{\g<cloze>}{\g<hint>}', t, flags=re.MULTILINE),
         # Convert code blocks to lstlisting in a given language, because pandoc cannot do it with "- " prefix
         lambda l: re.sub(r'\n- \`\`\`(\w*)\s*\n(- [^\`]+\n)+- \`\`\`', r'\n- \\begin{lstlisting}[style=knowledge_question,language=\1]\n\2- \\end{lstlisting}', l),
     ]
