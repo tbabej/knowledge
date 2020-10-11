76a4db98efc34d4205731c9683a483f24a7c5fc5
Tomas Babej <tomas@tbabej.com>
2020-10-10 21:32:13 -0400

    constants: Add PLUGIN_ROOT_DIR constant path

--- knowledge/constants.py
+++ knowledge/constants.py
@@ -2,4 +2,8 @@
 Contains plugin-level constants.
 """
 
+import pathlib
+
+
 ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PLUGIN_ROOT_DIR = pathlib.Path(__file__).absolute().parent.parent
